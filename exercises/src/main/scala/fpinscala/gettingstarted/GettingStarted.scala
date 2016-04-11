91e7f08e244c1d6ff8fabccd9c1f863b9deabbbc
Rob Martin <rmartin@tendrilinc.com>
2016-04-11 14:06:25 -0600

    chap 2 solutions done(ish)

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -36,7 +36,16 @@ object MyModule {
 
   // Exercise 1: Write a function to compute the nth fibonacci number
 
-  def fib(n: Int): Int = ???
+  def fib(n: Int): Int = {
+    def go(pp: Int, p: Int, i: Int): Int = {
+      if (i >= n) p
+      else go(p, pp + p, i + 1)
+    } 
+
+    if (n == 0) 0
+    else if (n == 1) 1
+    else go(0, 1, 1)
+  }
 
   // This definition and `formatAbs` are very similar..
   private def formatFactorial(n: Int) = {
@@ -140,7 +149,17 @@ object PolymorphicFunctions {
 
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = ???
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(a: A, as: Array[A]): Boolean =
+      if (as.isEmpty) true
+      else if (gt(as.head, a))
+        go(as.head, as.tail)
+      else false
+
+    if (as.isEmpty) true
+    else go(as.head, as.tail)
+  }
 
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
@@ -153,13 +172,13 @@ object PolymorphicFunctions {
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
   def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+    a => b => f(a,b)
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
   def uncurry[A,B,C](f: A => B => C): (A, B) => C =
-    ???
+    (a, b) => f(a)(b)
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -174,5 +193,16 @@ object PolymorphicFunctions {
   // Exercise 5: Implement `compose`
 
   def compose[A,B,C](f: B => C, g: A => B): A => C =
-    ???
+    a => f(g(a))
+}
+
+object TestIsSorted {
+  import PolymorphicFunctions.isSorted
+
+  def main(args: Array[String]) {
+    val gt: (Int, Int) => Boolean = (x, y) => x >= y
+    println(isSorted(Array(1, 2, 3), gt))
+    println(isSorted(Array(4, 1, 2, 3), gt))
+    println(isSorted(Array(), gt))
+  }
 }
